225c16d4ec587d63f11a6e944701c57a5cd2b6cc
Enache Octavian <enacheioan11@yahoo.com>
2022-04-27 13:00:50 +0300

    client server connection setting up

---
+++ client_server.py
@@ -0,0 +1,19 @@
+import socket
+
+
+
+class Server:
+    # we are connecting to the client from within the server
+    def __init__(self):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind(('127.0.0.1', 28000))
+        self.server_socket.listen(1)
+        self.conn_socket, self.client_address = self.server_socket.accept()
+
+
+
+class Client:
+    # we are connecting to the server from within the client
+    def __init__(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client_socket.connect(('127.0.0.1', 28000))
